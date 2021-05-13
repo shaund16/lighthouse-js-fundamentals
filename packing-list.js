b549dcb47eebe78a527a2f482a8535c43b724d2d
Shaun <shaundpurslow@gmail.com>
2021-05-13 20:05:16 +0000

    add a while loop in packing list file

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,18 @@
+//for loop 
+
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+
+// while loop 
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
