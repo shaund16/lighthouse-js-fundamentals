57c49dd9025a14f82f5f1b3a51840cc15442cbad
Shaun <shaundpurslow@gmail.com>
2021-05-13 03:01:24 +0000

    added even.js file and practiced more functions

--- hello.js
+++ hello.js
@@ -6,3 +6,12 @@ sayHello("Shaun");
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+console.log(greeting);
