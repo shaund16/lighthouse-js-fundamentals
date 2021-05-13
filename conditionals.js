275c54a1943eb430b340cebe999eba8c2bfa5d5e
Shaun <shaundpurslow@gmail.com>
2021-05-13 02:52:44 +0000

    added file hello and practiced functions

--- conditionals.js
+++ conditionals.js
@@ -1,30 +1,39 @@
 const temperature = -2;
 
-if(temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
+if (temperature < 0) {
+	console.log('Make sure you pick out a scarf!');
 } else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
+	console.log("Short sleeves won't cut it!");
 } else {
-  console.log("Short sleeves are fine.");
+	console.log('Short sleeves are fine.');
 }
-
 
 console.log("Now you're ready to go outside!");
 
-
-//Logical operators 
+//Logical operators
 
 const isCitizen = true;
 const age = 26;
 
 if (isCitizen && age > 18) {
-  console.log("You are eligible to vote.");
+	console.log('You are eligible to vote.');
 }
 
 if (temperature < -40 || temperature > 40) {
-  console.log("Maybe going outside isn't such a great idea…");
+	console.log("Maybe going outside isn't such a great idea…");
 }
 
 if (!raining) {
-  console.log("Leave your umbrella at home!");
+	console.log('Leave your umbrella at home!');
 }
+
+const whichSchool = function(age) {
+	if (age < 13) {
+		return 'Elementary School';
+	} else if (age >= 13 && age < 18) {
+		return 'Secondary School';
+	} else {
+		return 'Lighthouse Labs';
+	}
+};
+console.log(whichSchool(13));
